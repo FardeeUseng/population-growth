68990d486f6f9099719019ac7f7c67abccf94b82
Dee <fardee.tat@gmail.com>
2024-02-28 14:10:27 +0700

    -feat:create stats by using recharts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
+import Stats from './components/stats/Stats';
 
 function App() {
   return (
-    <div>
+    <div className='container'>
+      <h1>Population growth per country, 1950 to 2021</h1>
+      <h3>Click on the legend below to filter by continent</h3>
+      <div className='stats-wrap'>
+        <Stats/>
+      </div>
     </div>
   );
 }
