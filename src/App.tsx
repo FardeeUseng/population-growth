899d6ff191736c1d2b5fb8646fa5cce2096d1880
Dee <fardee.tat@gmail.com>
2024-02-28 15:17:28 +0700

    -feat:convert csv to json file for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import './App.css';
-import Stats from './components/stats/Stats';
+import "./App.css";
+import Stats from "./components/stats/Stats";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Population growth per country, 1950 to 2021</h1>
       <h3>Click on the legend below to filter by continent</h3>
-      <div className='stats-wrap'>
-        <Stats/>
+      <div className="reqion-wrap">
+        <h3>Region</h3>
+        {[
+          { label: "Asia", color: "" },
+          { label: "Europe", color: "" },
+          { label: "Affrica", color: "" },
+          { label: "Oceania", color: "" },
+          { label: "Americas", color: "" },
+        ].map((item, index) => (
+          <div key={index} className="reqion">
+            <div className="reqion-checkbox"></div>
+            <p>{item.label}</p>
+          </div>
+        ))}
+      </div>
+      <div className="stats-wrap">
+        <Stats />
       </div>
     </div>
   );
